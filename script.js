91c55b340f658fb9bb21a89f8ef0a92f884f16d1
grant <grant.enderkids@gmail.com>
2024-07-30 11:39:40 -0600

    updated the dice so that when a player rolls a 1, the game changes to the other player, and if and the bacground will toggle between the active player

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +17,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 //rolling the dice btn--new, btn--roll, btn--hold
 btnRoll.addEventListener('click', function () {
   //1. generating a random dice roll
@@ -20,5 +28,18 @@ btnRoll.addEventListener('click', function () {
   //2 display the dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  //3. checked for rolled 1. if tru switch players
+  //3. checked for rolled 1. if true switch players
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 });
